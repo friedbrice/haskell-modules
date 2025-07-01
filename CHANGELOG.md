10aa21c88c062d8916c5cb9452515413c46dfb5b
Daniel Brice <danielbrice@gmail.com>
2025-07-01 12:44:20 -0700

    Fix bug in config path 'excludesRegEx'

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,7 +4,9 @@ All notable changes to the "haskell-modules" extension will be documented in thi
 
 Check [Keep a Changelog](http://keepachangelog.com/) for recommendations on how to structure this file.
 
-## [Unreleased]
+## [1.0.1]
+
+- Fix bug that prevented parsing of config path `haskell-modules.excludesRegEx`
 
 ## [1.0.0]
 
--- src/config.ts
+++ src/config.ts
@@ -7,15 +7,20 @@ export type Config = {
 }
 
 export namespace Config {
+  const revealFocusedPath = 'revealFocused'
   const defaultRevealFocused = true
+
+  const hydratePrefixPath = 'hydratePrefix'
   const defaultHydratePrefix = 'HM_'
+
+  const excludesRegExpPath = 'excludesRegEx'
   const defaultExcludesRegExpRaw = '(.stack-work\\/|dist\\/|dist-newstyle\\/)'
 
   export function create(): Config {
     const hsModsSection = vscode.workspace.getConfiguration('haskell-modules')
-    const revealFocused = hsModsSection.get<boolean>('revealFocused', defaultRevealFocused)
-    const hydratePrefix = hsModsSection.get<string>('hydratePrefix', defaultHydratePrefix)
-    const excludesRegExpRaw = hsModsSection.get<string>('excludeRegex', defaultExcludesRegExpRaw)
+    const revealFocused = hsModsSection.get<boolean>(revealFocusedPath, defaultRevealFocused)
+    const hydratePrefix = hsModsSection.get<string>(hydratePrefixPath, defaultHydratePrefix)
+    const excludesRegExpRaw = hsModsSection.get<string>(excludesRegExpPath, defaultExcludesRegExpRaw)
     const excludesRegExp = new RegExp(excludesRegExpRaw)
     return { revealFocused, hydratePrefix, excludesRegExp }
   }
